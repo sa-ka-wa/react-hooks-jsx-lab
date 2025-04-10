a40979f8cbbad446c08984574008e084af13b926
sa-ka-wa <samiekaranjawainaina@gmail.com>
2025-04-10 15:18:18 +0300

    Refactor components to return structured JSX in About, Home, and NavBar

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,17 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I am a full-stack web developer with a passion for creating dynamic and
+        responsive web applications. I enjoy working with the latest
+        technologies and continuously learning new skills to improve my craft.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
